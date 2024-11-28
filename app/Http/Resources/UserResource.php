86e170fdc63562151b929bf5d75b247ad368ac1f
Syfur007 <syfur.cse8.bu@gmail.com>
2024-11-28 22:05:22 +0600

    Create User Resources

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public static $wrap = false;
+    /**
+     * Information about a user that is shown to other users ie. in a question, profile.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+        ];
+    }
+}
